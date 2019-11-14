2f9d665255fd86d45e4c53aa615535a2675ea3e1
Otavio Jacobi <otaviojacobi@gmail.com>
2019-11-14 16:37:05 +0100

    version with sequelize

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const faker = require('faker');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const sqlite3 = require('sqlite3').verbose();
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 // Initialisation serveur
 const app = express();
@@ -42,20 +42,28 @@ app.delete('/paris/:id', (req, res) => {
 
 /* Partie SQLite */
  // Connexion à la base de donnée avec sqlite
-const db = new sqlite3.Database('database-phe.db');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database-phe.db'
+});
 
 // Initialisation de la table paris
-db.run('CREATE TABLE IF NOT EXISTS paris(id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, cheval INTEGER, date TEXT)');
+class Paris extends Model {}
+Paris.init({
+  author: DataTypes.STRING,
+  cheval: DataTypes.INTEGER,
+}, { sequelize, modelName: 'paris', createdAt: 'date' });
+Paris.sync();
 
 // Récupération de tous les paris avec Sqlite
 const getAllParis = (req, res) => {
-  db.all('SELECT rowid as id, * FROM paris ORDER BY date', [], (err, rows) => {
-    if(err) {
-      console.error(err);
-      return res.status(503).send('Nous avons eu un problème de connexion à la base de données');
-    }
-    return res.status(200).send(rows);
-  });
+
+  Paris.findAll({attributes: {exclude: ['updatedAt']} })
+       .then(rows => res.status(200).send(rows))
+       .catch(err => {
+          console.error(err);
+          return res.status(503).send('Nous avons eu un problème de connexion à la base de données');
+       });
 };
 
 // Ajout d'un pari avec Sqlite
@@ -65,14 +73,12 @@ const insertNewParis = (req, res) => {
       return res.status(400).send('Le body doit contenir "author" et "cheval". Le premier doit être une chaîne de caractères et le second un entier.');
   }
 
-  const date = new Date().toISOString();
-  db.run('INSERT INTO paris(author, cheval, date) VALUES (?, ?, ?)', [req.body.author, req.body.cheval, date], err => {
-    if(err) {
-      console.error(err);
-      return res.status(503).send('Echoué pour créé Pari cause d\'un problème de connexion à la base de données');
-    } 
-    return res.status(201).send('Pari créé');
-  });
+  return Paris.create({ author: req.body.author, cheval: req.body.cheval})
+              .then(_ => res.status(201).send('Pari créé'))
+              .catch(err => {
+                console.error(err);
+                return res.status(503).send('Echoué pour créé Pari cause d\'un problème de connexion à la base de données');
+              });
 };
 
 const _isNewParisBodyValid = body => {
@@ -87,37 +93,27 @@ const _isNewParisBodyValid = body => {
 // Génération de 10 paris aléatoires avec Sqlite
 const generateParis = (req, res, amount) => {
   
-  const parises = [];
+  const paris = [];
   for(let i = 0; i < amount; i++) {
-    parises.push([faker.name.firstName(), faker.random.number({min: 1, max: 10}), new Date().toISOString()]);
+    paris.push({author: faker.name.firstName(), cheval: faker.random.number({min: 1, max: 10}) });
   }
 
-  const placeholders = parises.map(_ => '(?, ?, ?)').join(',');
-  const query = 'INSERT INTO paris (author, cheval, date) VALUES ' + placeholders;
-  db.run(query, [].concat.apply([], parises),  err => {
-    if(err) {
-      console.error(err);
-      return res.status(503).send('Echoué pour créé Paries cause d\'un problème de connexion à la base de données');
-    } 
-    return res.status(201).send('10 Paris générés aléatoirement');
-  });
+  return Paris.bulkCreate(paris)
+              .then(_ => res.status(201).send('10 Paris générés aleatoirement'))
+              .catch(err => {
+                console.error(err);
+                res.status(503).send('Echoué pour créé Pari cause d\'un problème de connexion à la base de données');
+              });
 };
 
 // Suppression d'un pari avec Sqlite
 const deleteParis = (req, res) => {
-  db.run('DELETE FROM paris WHERE id=?', req.params.id, function(err) {
-
-    if(err) {
-      console.error(err);
-      return res.status(503).send('Echoué pour supprimer Paris cause d\'un problème de connexion à la base de données');
-    }
-
-    if(this.changes === 0) {
-      return res.status(400).send('Ce paris doest n\'existe pas');
-    }
-
-    return res.status(200).send(`Paris avec ID ${req.params.id} supprimé`);
-  });
+  return Paris.destroy({where: { id : req.params.id }})
+              .then(deleted => deleted ? res.status(200).send('Pari supprimé') : res.status(400).send('N\'a trouvé paris avec cette id'))
+              .catch(err => {
+                console.error(err);
+                res.status(503).send('Echoué pour créé Pari cause d\'un problème de connexion à la base de données');
+              });
 };
 
 /* Démarrage serveur */
